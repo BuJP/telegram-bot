f2ada2a89d4964576b9e3838ef791f3b8818708b
BuJP <jeanphilippe.buffon@gmail.com>
2024-03-16 12:17:04 +0100

    feat: health check

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,12 @@ import { TelegramBotService } from "./service/telegramBotService";
 const app = express();
 const environmentVariableService = new EnvironmentVariableService();
 const bot = new TelegramBotService(environmentVariableService);
+
 bot.start(app);
 
 const PORT = environmentVariableService.get("PORT");
+
+app.get("/", (_, res) => res.send("App is running..."));
 
 app.listen(PORT, () => console.log("Listening on port", PORT));
 
